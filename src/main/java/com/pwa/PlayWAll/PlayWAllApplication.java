16f647e4a1fe0d8712deb2ac3bebbfc9d94979a3
Colunga-D <xkould@gmail.com>
2024-03-20 17:57:42 +0100

    first commit

---
+++ src/main/java/com/pwa/PlayWAll/PlayWAllApplication.java
@@ -0,0 +1,13 @@
+package com.pwa.PlayWAll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayWAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pwa/PlayWAll/PlayWAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwa.PlayWAll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayWAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
